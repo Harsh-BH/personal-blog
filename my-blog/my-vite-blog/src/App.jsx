c8c4cfb39a4feb429b8f3c0edf138808667922ae
Harsh-BH <162444490+Harsh-BH@users.noreply.github.com>
2024-07-12 12:27:02 +0530

    v5

--- my-blog/my-vite-blog/src/App.jsx
+++ my-blog/my-vite-blog/src/App.jsx
@@ -1,5 +1,4 @@
-// App.js
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Shops from "./pages/Shops";
@@ -10,21 +9,19 @@ import "./styles/globals.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/shops" element={<Shops />} />
-        <Route path="/profile" element={<Profile />} />
-        {blogPosts.map((post) => (
-          <Route
-            key={post.id}
-            path={`/blog/${post.id}`}
-            element={<BlogPage blog={post} />}
-          />
-        ))}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/shops" element={<Shops />} />
+      <Route path="/profile" element={<Profile />} />
+      {blogPosts.map((post) => (
+        <Route
+          key={post.id}
+          path={`/blog/${post.id}`}
+          element={<BlogPage blog={post} />}
+        />
+      ))}
+    </Routes>
   );
 }
 
